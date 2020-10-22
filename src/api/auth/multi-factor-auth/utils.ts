5450124e2aaf548c1a68f95c8de12c78b99a2cc4
darkfusion90 <gauravsharmaa341@gmail.com>
2020-10-22 20:33:26 +0530

    Added multifactor util: check if current device is verified

--- src/api/auth/multi-factor-auth/utils.ts
+++ src/api/auth/multi-factor-auth/utils.ts
@@ -1,9 +1,23 @@
 import { IDevice } from '../../../database/models/TrustedDeviceModel'
 import { Request } from 'express'
+import { isDeviceTrusted } from '../../../database/controllers/trusted-devices'
+import { sessionUserId } from '../../../utils/session-user'
 
 export const getUserDeviceInfo = (req: Request<any, any, any, any>): IDevice => {
     return {
         userAgent: req.headers['user-agent'] || 'unknown',
         ipAddress: req.ip
     }
+}
+
+export const isCurrentDeviceTrusted = (
+    req: Request<any, any, any, any>,
+    userId?: string
+): Promise<boolean> => {
+    const _userId = sessionUserId(req) || userId
+    if (!_userId) {
+        return Promise.reject('Session missing in req object OR userId not provided')
+    }
+
+    return isDeviceTrusted(_userId, getUserDeviceInfo(req))
 }
