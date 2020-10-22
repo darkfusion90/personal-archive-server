97b059b2697c40efd2cb8f7158f4e80899bbca6f
darkfusion90 <gauravsharmaa341@gmail.com>
2020-10-22 14:19:07 +0530

    Added util: sessionUserId to get userId of current user

--- src/utils/session-user.ts
+++ src/utils/session-user.ts
@@ -1,6 +1,11 @@
 import { Request } from "express";
 import { IUserDocument } from '../database/models/UserModel'
 
+export const sessionUserId = (req: Request<any, any, any, any>): string | null => {
+    const user = sessionUser(req)
+    return user ? user.id : null
+}
+
 export default function sessionUser(req: Request<any, any, any, any>): IUserDocument | null {
     return req.user as IUserDocument
 }
