d8af4b563a4006151a5fb9b6db0ee509aa8dd08f
darkfusion90 <gauravsharmaa341@gmail.com>
2020-10-22 07:07:26 +0530

    Added `update` to exports

--- src/database/controllers/posts/index.ts
+++ src/database/controllers/posts/index.ts
@@ -1,9 +1,11 @@
 export * from './create'
 export * from './retrieve'
 export * from './delete'
+export * from './update'
 
 import * as create from './create'
 import * as retrieve from './retrieve'
 import * as _delete from './delete'
+import * as update from './update'
 
-export default { ...create, ...retrieve, ..._delete }
+export default { ...create, ...retrieve, ..._delete, ...update }
