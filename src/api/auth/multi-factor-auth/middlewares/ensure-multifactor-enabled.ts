d886ddac11413e46d8e5d80766f55168dcda5852
darkfusion90 <gauravsharmaa341@gmail.com>
2020-10-22 20:33:43 +0530

    Refactored to fix typos

--- src/api/auth/multi-factor-auth/middlewares/ensure-multifactor-enabled.ts
+++ src/api/auth/multi-factor-auth/middlewares/ensure-multifactor-enabled.ts
@@ -3,7 +3,7 @@ import { RequestHandler } from 'express'
 import { hasUserEnabledMultifactorAuth } from '../../../../database/controllers/auth-details'
 import { sessionUserId } from '../../../../utils/session-user'
 
-const ensureEmailVerified: RequestHandler = async (req, res, next) => {
+const ensureMultifactorAuthEnabled: RequestHandler = async (req, res, next) => {
     const userId = sessionUserId(req)
     if (!userId) {
         return res.status(401).json({ message: 'missing session' })
@@ -12,9 +12,9 @@ const ensureEmailVerified: RequestHandler = async (req, res, next) => {
     if (await hasUserEnabledMultifactorAuth(userId)) {
         next()
     } else {
-        // TODO: Throw error here (EmailNotVerifiedError) and handle in global error handler
+        // TODO: Throw error here (MultifactorNotEnabledError) and handle in global error handler
         return res.status(401).json({ message: 'multifactor auth required' })
     }
 }
 
-export default ensureEmailVerified
+export default ensureMultifactorAuthEnabled
