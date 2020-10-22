0ab3503f20fb6d6b5b09d29009afe321498904c0
darkfusion90 <gauravsharmaa341@gmail.com>
2020-10-22 20:31:20 +0530

    refactored imports

--- src/api/auth/register.ts
+++ src/api/auth/register.ts
@@ -1,13 +1,13 @@
 import RegisterHandler from './typings/register'
 
 import { createUser } from '../../database/controllers/users'
-import { performLogin } from './utils'
+import { performLoginRequestHandler } from './utils'
 
 const register: RegisterHandler = (req, res, next) => {
     const { username, email, password } = req.body
     createUser({ username, email, password })
         .then((user) => {
-            performLogin(user)(req, res, next)
+            performLoginRequestHandler(user)(req, res, next)
         })
         .catch(next)
 }
