2283c3ce187ddc9e6dcf1d9068c7ce7774d27fc1
darkfusion90 <gauravsharmaa341@gmail.com>
2020-10-24 13:32:39 +0530

    Added feature to mail the generated auth token to the user's mail id

--- src/api/auth/multi-factor-auth/generate-token.ts
+++ src/api/auth/multi-factor-auth/generate-token.ts
@@ -1,17 +1,36 @@
 import { RequestHandler } from 'express'
 
-import { sessionUserId } from '../../../utils/session-user'
+import { sessionUser, getAbsoluteUrl, simpleParseUseragent } from '../../../utils'
 import { createAuthToken } from '../../../database/controllers/auth-tokens'
 import { getUserDeviceInfo } from './utils'
+import { sendDeviceVerificationMail } from '../../../services/mail'
+import {
+    ensureMultifactorAuthEnabled,
+    removeExistingAuthTokens
+} from './middlewares'
+import { ensureLoggedIn } from '../middlewares'
+import { IUserDocument } from '../../../database/models/UserModel'
 
 const generateToken: RequestHandler = async (req, res) => {
-    const userId = sessionUserId(req)
-    if (!userId) {
-        return res.status(500).json({ message: 'session missing' })
-    }
+    const user = sessionUser(req) as IUserDocument
 
-    const authToken = await createAuthToken(userId, getUserDeviceInfo(req))
-    res.json({ authToken })
+    const userDeviceInfo = getUserDeviceInfo(req)
+    const authToken = await createAuthToken(user.id, userDeviceInfo)
+
+    await sendDeviceVerificationMail(user, {
+        // Have a DEVICE_VERIFY_CALLBACK in process.env
+        verificationLink: getAbsoluteUrl(`/api/auth/multifactor/verify/${authToken.token}`),
+        deviceInfo: simpleParseUseragent(userDeviceInfo.userAgent),
+        ipAddress: userDeviceInfo.ipAddress
+    })
+
+    res.json({ message: 'token generated' })
 }
 
-export default generateToken
+export default [
+    // TODO: Add ensureEmailVerified
+    ensureLoggedIn({ checkMultifactor: false }),
+    ensureMultifactorAuthEnabled,
+    removeExistingAuthTokens,
+    generateToken
+]
