d2cd769406cf1d6c828b89c0b44bb9d146371568
darkfusion90 <gauravsharmaa341@gmail.com>
2020-10-25 07:37:58 +0530

    Removed experimental tweaking of expiresAt path

--- src/database/models/PasswordResetTokenModel.ts
+++ src/database/models/PasswordResetTokenModel.ts
@@ -5,12 +5,6 @@ export interface IPasswordResetTokenDocument extends ITokenDocument { }
 
 export const PasswordResetTokenSchema = new mongoose.Schema<IPasswordResetTokenDocument>({}, { discriminatorKey })
 
-PasswordResetTokenSchema.pre<IPasswordResetTokenDocument>('save', function (next) {
-    this.expiresAt = new Date(2099, 12, 31, 23, 59, 59)
-    this.markModified('expiresAt')
-    next()
-})
-
 export const PasswordResetTokenModel = TokenModel.discriminator<IPasswordResetTokenDocument>(
     'PasswordResetToken',
     PasswordResetTokenSchema,
