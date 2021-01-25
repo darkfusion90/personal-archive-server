4af2f22624e08402bbef2e04875398128e6a8ce1
darkfusion90 <gauravsharmaa341@gmail.com>
2021-01-25 19:00:20 +0530

    Added code and imports to send email upon token generation Added middleware to remove existing tokens every time generate token is called

--- src/api/auth/email-verification/generate-email-token.ts
+++ src/api/auth/email-verification/generate-email-token.ts
@@ -2,20 +2,29 @@ import { RequestHandler } from 'express'
 
 import asyncHandler from '../../async-handler'
 import { checkIfEmailAlreadyVerified } from './check-if-email-already-verified'
-import { sessionUserId } from '../../../utils'
+import { sessionUser } from '../../../utils'
 import { createEmailVerificationToken } from '../../../database/controllers/email-verification-tokens'
 import { ensureLoggedIn } from '../middlewares'
-//import { sendEmailConfirmationMail } from '../../../services/mail'
+import { sendEmailConfirmationMail } from '../../../services/mail'
+import { removeExistingEmailTokens } from './remove-existing-email-tokens'
+import { IUserDocument } from '../../../database/models/UserModel'
 
 const generateEmailToken: RequestHandler = async (req, res) => {
-    const userId = sessionUserId(req) as string
-    const token = await createEmailVerificationToken(userId)
+    const user = sessionUser(req) as IUserDocument
+    const token = await createEmailVerificationToken(user.id)
 
+    /*await sendEmailConfirmationMail(
+        user,
+        `/just-a-link/${token.token}`
+    )*/
+
+    console.log({ token })
     res.json({ token })
 }
 
 export default [
     ensureLoggedIn({ checkMultifactor: false }),
     checkIfEmailAlreadyVerified,
+    removeExistingEmailTokens,
     asyncHandler(generateEmailToken)
 ]
