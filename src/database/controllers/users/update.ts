30f1b47c952446af19f163f000827a3332c43b1a
darkfusion90 <gauravsharmaa341@gmail.com>
2021-01-25 18:57:01 +0530

    Added db method to update user details

--- src/database/controllers/users/update.ts
+++ src/database/controllers/users/update.ts
@@ -1,5 +1,24 @@
+import _ from 'lodash'
+import { markEmailVerifiedForUser, markMultifactorAuthForUser } from '../auth-details'
 import UserModel from '../../models/UserModel'
+
+type UserEditableField = 'username' | 'email'
+export type UserEditableFieldMap = Record<UserEditableField, string>
 
 export const updateUserPassword = (userId: string, password: string) => {
     return UserModel.findByIdAndUpdate(userId, { password }, { new: true }).exec()
+}
+
+export const updateUserDetails = async (userId: string, toUpdate: Partial<UserEditableFieldMap>) => {
+    // removes null/undefined paths
+    const updateQuery = _.omitBy(toUpdate, _.isNil) as Partial<UserEditableFieldMap>
+
+    if (updateQuery.email) {
+        await Promise.all([
+            markEmailVerifiedForUser(userId, false),
+            markMultifactorAuthForUser(userId, false)
+        ])
+    }
+
+    return UserModel.findByIdAndUpdate(userId, updateQuery, { new: true })
 }
