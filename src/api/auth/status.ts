7854ec4552fb9b3ffe8a2e16795a915872694f20
darkfusion90 <gauravsharmaa341@gmail.com>
2021-05-26 20:13:07 +0530

    Removed redundant condition

--- src/api/auth/status.ts
+++ src/api/auth/status.ts
@@ -1,15 +1,10 @@
-import { RequestHandler } from 'express';
+import { RequestHandler } from "express";
 
-import { getUserAuthStatus } from './utils';
+import { getUserAuthStatus } from "./utils";
 
 const status: RequestHandler = async (req, res) => {
-    const authStatus = await getUserAuthStatus(req)
-    
-    if (authStatus.loggedIn) {
-        res.json(authStatus)
-    } else {
-        res.json({ loggedIn: false })
-    }
-}
+  const authStatus = await getUserAuthStatus(req);
+  res.json(authStatus);
+};
 
-export default status
+export default status;
